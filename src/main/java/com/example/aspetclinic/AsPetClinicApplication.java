d5272e574d0a52f427f2e180133dd99337712a89
AbuSalam22 <abu.sufian.salam@hotmail.com>
2023-06-19 10:18:50 +0100

    Initial Project Add.

---
+++ src/main/java/com/example/aspetclinic/AsPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.example.aspetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/aspetclinic/AsPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aspetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
